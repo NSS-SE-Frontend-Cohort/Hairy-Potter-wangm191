fcff7e992d658b3c190823d48a32163508862ad0
Myles Wang <formyleswang@gmail.com>
2025-01-24 05:50:21 -0500

    implemented makePottery() and called it 5 times, creating 5 pottery objects

---
+++ src/scripts/PotteryWheel.js
@@ -0,0 +1,13 @@
+let primaryKey = 1
+
+export const makePottery = (shape, weight, height) => {
+    const pottery = {
+        shape: shape,
+        weight: weight, 
+        height: height,
+        id: primaryKey
+    }
+    primaryKey ++;
+
+    return pottery;
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,18 @@
 // Imports go first
-
-
+import { makePottery } from "./PotteryWheel.js"
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 1, 3)
+let plate = makePottery("plate", 2, 1)
+let bowl = makePottery("bowl", 2, 2)
+let pan = makePottery("pan", 5, 3)
+let tray = makePottery("tray", 4, 1)
 
+console.log(mug)
+console.log(plate)
+console.log(bowl)
+console.log(pan)
+console.log(tray)
 
 // Fire each piece of pottery in the kiln
 
