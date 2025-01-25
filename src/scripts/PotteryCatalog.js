c05580ba1548a6e19eb3a90124e28bad5cd02dc6
Myles Wang <formyleswang@gmail.com>
2025-01-25 16:01:59 -0500

    implemented toSellOrNotToSell() and usePottery(), adding price and returning sold objects

---
+++ src/scripts/PotteryCatalog.js
@@ -0,0 +1,17 @@
+const soldPottery = []
+
+export const toSellOrNotToSell = (pottery) => {
+    if ( pottery.cracked == false ){
+        if ( pottery.weight >= 6 ) {
+            pottery.price = 40
+        }
+        else {
+            pottery.price = 20
+        }
+        soldPottery.push(pottery)
+    }
+
+    return pottery
+}
+
+export const usePottery = () => JSON.parse(JSON.stringify(soldPottery));
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,13 @@
 // Imports go first
 import { firePottery } from "./Kiln.js"
+import { toSellOrNotToSell, usePottery } from "./PotteryCatalog.js"
 import { makePottery } from "./PotteryWheel.js"
 
 // Make 5 pieces of pottery at the wheel
 const mug = makePottery("mug", 1, 3)
 const plate = makePottery("plate", 2, 1)
-const bowl = makePottery("bowl", 2, 2)
-const pan = makePottery("pan", 5, 3)
+const bowl = makePottery("bowl", 10, 5)
+const pan = makePottery("pan", 7, 3)
 const tray = makePottery("tray", 4, 1)
 
 console.log(mug)
@@ -30,6 +31,19 @@ console.log(fireTray)
 
 // Determine which ones should be sold, and their price
 
+const sellMug = toSellOrNotToSell(mug)
+const sellPlate = toSellOrNotToSell(plate)
+const sellBowl = toSellOrNotToSell(bowl)
+const sellPan = toSellOrNotToSell(pan)
+const sellTray = toSellOrNotToSell(tray)
+
+console.log(sellMug)
+console.log(sellPlate)
+console.log(sellBowl)
+console.log(sellPan)
+console.log(sellTray)
+const soldPottery = usePottery()
+console.log(soldPottery)
 
 // Invoke the component function that renders the HTML list
 
